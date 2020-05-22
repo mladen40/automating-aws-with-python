3cd1a38065afdd389a595f45e5fc847d77c5c907
mladen40 <bojanic@macbook15.localdomain>
2020-05-21 23:32:57 -0600

    Set up CloudFront CDN and SSL.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -22,7 +22,6 @@ from webotron.cdn import DistributionManager
 
 from webotron import util
 
-
 session = None
 bucket_manager = None
 domain_manager = None
@@ -35,7 +34,7 @@ dist_manager = None
               help="Use a given AWS profile.")
 def cli(profile):
     """Webotron deploys websites to AWS."""
-    global session, bucket_manager, domain_manager
+    global session, bucket_manager, domain_manager, cert_manager, dist_manager
 
     session_cfg = {}
     if profile:
@@ -58,7 +57,7 @@ def list_buckets():
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    """List object in an s3 bucket."""
+    """List objects in an s3 bucket."""
     for obj in bucket_manager.all_objects(bucket):
         print(obj)
 
@@ -89,11 +88,12 @@ def setup_domain(domain):
     """Configure DOMAIN to point to BUCKET."""
     bucket = bucket_manager.get_bucket(domain)
 
-    zone = domain_manager.find_hosted_zone(domain) or domain_manager.create_hosted_zone(domain)
+    zone = domain_manager.find_hosted_zone(domain) \
+        or domain_manager.create_hosted_zone(domain)
 
     endpoint = util.get_endpoint(bucket_manager.get_region_name(bucket))
     domain_manager.create_s3_domain_record(zone, domain, endpoint)
-    print("Domain configure: http://{}".format(domain))
+    print("Domain configured: http://{}".format(domain))
 
 
 @cli.command('find-cert')
